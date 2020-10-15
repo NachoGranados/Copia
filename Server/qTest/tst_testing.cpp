1deb32e15450ddc4aadb5311e6c259578a2d88e5
Juan Ignacio Navarro <ju.navarro@estudiantec.cr>
2020-10-15 17:31:40 -0600

    Update Integration tests

--- Server/qTest/tst_testing.cpp
+++ Server/qTest/tst_testing.cpp
@@ -11,6 +11,7 @@ public:
 
 private slots:
 
+    //Unit testing in the server
     void test_actualEdges();
 
     void test_findEdge();
@@ -21,11 +22,22 @@ private slots:
 
     void test_printPath();
 
-    void test_printSolution();
+    void test_printSolution02();
 
     void test_floydWarshall();
 
+    //Integrations tests
+    //They will test all the program integration woth the final result given bythe server
+
+    void test_printSolution31();
+
+    void test_printSolution01();
+
+    void test_printSolution03();
+
 };
+
+//Variables needed for the unit testing
 
 QVector< QVector<int> > edges = { { 0, 1, 3 }, { 0, 3, 5 }, { 1, 0, 2 }, { 1, 3, 4 }, { 2, 1, 1 }, { 3, 2, 2 } };
 
@@ -38,11 +50,22 @@ int nodes = 4;
 QVector< QVector<int> > floydWarshallMatrix = { {0, 3, infinite, 5}, {2, 0, infinite, 4},
                                                 {infinite, 1, 0, infinite}, {infinite, infinite, 2 , 0} };
 
-QString message = "Floyd Warshall Algorithm\n\nThe shortest path from 0 -> 2 is (0-3-2)\n";
+QString message02 = "Floyd Warshall Algorithm\n\nThe shortest path from 0 -> 2 is (0-3-2)\n";
+
 
 QVector< QVector<int> > path = { {0, 0, 3, 0}, {-1, 0, 3, -1}, {-1, 2, 0, -1}, {-1, 2, 3, 0} };
 
 QVector<int> actualEdgesNode = {1,3};
+
+//Variables needed for the instegration tests
+
+QString message31 = "Floyd Warshall Algorithm\n\nThe shortest path from 3 -> 1 is (3-2-1)\n";
+
+QString message01 = "Floyd Warshall Algorithm\n\nThe shortest path from 0 -> 1 is (0-1)\n";
+
+QString message03 = "Floyd Warshall Algorithm\n\nThe shortest path from 0 -> 3 is (0-3)\n";
+
+//Unit testing method implementation
 
 void Testing::test_actualEdges() {
 
@@ -88,11 +111,11 @@ void Testing::test_printPath() {
 
 }
 
-void Testing::test_printSolution() {
+void Testing::test_printSolution02() {
 
     QString result = graph.printSolution(path, nodes, 0, 2);
 
-    QCOMPARE(result, message);
+    QCOMPARE(result, message02);
 
 }
 
@@ -100,9 +123,30 @@ void Testing::test_floydWarshall() {
 
     QString result = graph.floydWarshall(floydWarshallMatrix, nodes, infinite, 0, 2);
 
-    QCOMPARE(result, message);
+    QCOMPARE(result, message02);
 
 }
+
+//Métodos de las pruebas de integración
+
+void Testing::test_printSolution31(){
+    QString result = graph.printSolution(path, nodes, 3, 1);
+
+    QCOMPARE(result, message31);
+}
+
+void Testing::test_printSolution01(){
+    QString result = graph.printSolution(path, nodes, 0, 1);
+
+    QCOMPARE(result, message01);
+}
+
+void Testing::test_printSolution03(){
+    QString result = graph.printSolution(path, nodes, 0, 3);
+
+    QCOMPARE(result, message03);
+}
+
 
 QTEST_MAIN(Testing)
 
